49c5a779c8120b7cf8fd0a6c6644ae15db25f6f0
Zoltan Horvath <zoltan.horvath@cheppers.com>
2018-12-11 13:50:56 +0100

    Skip file input tests for emulated mobile devices - since file upload not supported

--- tests/Nightwatch/Tests/textScreenshots.js
+++ tests/Nightwatch/Tests/textScreenshots.js
@@ -27,9 +27,17 @@ module.exports = {
     const imagePath = path.join(__dirname, '..', '..', 'assets', 'test.gif');
     const fileInvalidPath = path.join(__dirname, '..', '..', 'assets', 'testBig.txt');
     const imageInvalidPath = path.join(__dirname, '..', '..', 'assets', 'test.png');
+    let fileTest = true; // See first perform command.
 
     ['', 'he'].forEach((langprefix) => {
       browser
+        .perform(() => {
+          // Skip file upload tests on mobileEmulation since it's not supported.
+          const platformName = (browser.capabilities.platformName || browser.capabilities.platform || 'nan').toLowerCase();
+          const browserName = (browser.capabilities.browserName || 'nan').toLowerCase();
+          const unsupported = ['chrome:android', 'safari:ios'];
+          fileTest = unsupported.indexOf(`${browserName}:${platformName}`) < 0;
+        })
         .resizeWindow(1024, 600)
         .sevenRefreshURL((langprefix ? '/' + langprefix : '') + '/contact/textform')
         // Text.
@@ -61,29 +69,39 @@ module.exports = {
         .setValueAndChange('[name="daterange_ad[0][end_value][date]"]', '2018-10-28')
         // Color.
         .setValueAndChange('[name="color[0][value]"]', '#ff0000')
-        // Valid image file.
-        .setValueAndChange('[name="files[image_0]"]:not([disabled])', imagePath)
-        .waitTillElementPresent('[name="image[0][alt]"]', 5000)
-        .setValueAndChange('[name="image[0][alt]"]', 'Green square')
-        // Valid file.
-        .setValueAndChange('[name="files[file_0]"]:not([disabled])', filePath)
-        .waitTillElementPresent('[name="file[0][description]"]', 5000)
-        .setValueAndChange('[name="file[0][description]"]', 'File description')
+        .perform(() => {
+          if (fileTest) {
+            // Valid image file.
+            browser
+            .setValueAndChange('[name="files[image_0]"]:not([disabled])', imagePath)
+            .waitTillElementPresent('[name="image[0][alt]"]', 5000)
+            .setValueAndChange('[name="image[0][alt]"]', 'Green square')
+            // Valid file.
+            .setValueAndChange('[name="files[file_0]"]:not([disabled])', filePath)
+            .waitTillElementPresent('[name="file[0][description]"]', 5000)
+            .setValueAndChange('[name="file[0][description]"]', 'File description')
+          }
+        })
         .savefullScreenShot('01', langprefix)
         .click('input#edit-submit')
         // Waiting for error messages (Big Pipe).
         .waitTillElementPresent('.messages--error', 5000)
-        // Remove image and file.
-        .click('[name="file_0_remove_button"]')
-        .click('[name="image_0_remove_button"]')
-        .waitTillElementPresent('[name="files[file_0]"]:not([disabled])', 5000)
-        // Upload invalid file.
-        .setValueAndChange('[name="files[file_0]"]', fileInvalidPath)
-        .waitTillElementPresent('[name="files[file_0]"].error', 5000)
-        // Upload invalid image.
-        .waitTillElementPresent('[name="files[image_0]"]:not([disabled])', 5000)
-        .setValueAndChange('[name="files[image_0]"]', imageInvalidPath)
-        .waitTillElementPresent('[name="files[image_0]"].error', 5000)
+        .perform(() => {
+          if (fileTest) {
+            browser
+              // Remove image and file.
+              .click('[name="file_0_remove_button"]')
+              .click('[name="image_0_remove_button"]')
+              .waitTillElementPresent('[name="files[file_0]"]:not([disabled])', 5000)
+              // Upload invalid file.
+              .setValueAndChange('[name="files[file_0]"]', fileInvalidPath)
+              .waitTillElementPresent('[name="files[file_0]"].error', 5000)
+              // Upload invalid image.
+              .waitTillElementPresent('[name="files[image_0]"]:not([disabled])', 5000)
+              .setValueAndChange('[name="files[image_0]"]', imageInvalidPath)
+              .waitTillElementPresent('[name="files[image_0]"].error', 5000)
+          }
+        })
         // Create 'Error' screenshot.
         .elements('css selector', '.form-item__error-message,.form-item--error-message', (results) => {
           browser.savefullScreenShot((results.value.length ? '03' : '02'), langprefix, (results.value.length ? 'Inline error states' : 'Error states'));
